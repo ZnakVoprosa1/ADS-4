d25130849b0e7f4a76ba8559317426bb12d1c08d
Степан <184nikiforov7b@mail.ru>
2025-06-07 01:36:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,8 +5,7 @@ int countPairs1(int *arr, int len, int value) {
     for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value) {
         count++;
-      }
-      else if (arr[i] + arr[j] > value) {
+      } else if (arr[i] + arr[j] > value) {
         break;
       }
     }
@@ -14,21 +13,29 @@ int countPairs1(int *arr, int len, int value) {
   return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  int left = 0;
-  int right = len - 1;
-  int count = 0;
+  int result = 0;
+  int left = 0, right = len - 1;
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum < value) {
-      left++;
-    }
-    else if (sum > value) {
-      right--;
-    }
-    else {
-      count++;
-      left++;
-      right--;
+      int l = left, r = right;
+      int leftCount = 1;
+      int leftCount = 1;
+      while(l + 1 < r && arr[l + 1] == arr[left]) {++l; ++leftCount;}
+      while(r - 1 > l && arr[r - 1] == arr[right]) {--r; ++rightCount;}
+      if(arr[left] == arr[right]) {
+        int n = right - left + 1;
+        result += n * (n - 1) / 2;
+        break;
+      } else if (sum > value) {
+        result += leftCount * rightCount;
+        left = l + 1;
+        right = r - 1;
+      }
+    } else if(sum < value) {
+      ++left;
+    } else {
+      --right;
     }
   }
   return count;
@@ -47,11 +54,9 @@ int countPairs3(int *arr, int len, int value) {
         right_bound++;
         count += (right_bound - left_bound + 1);
         break;
-      }
-      else if (arr[m] < target) {
+      } else if (arr[m] < target) {
         l = m + 1;
-      }
-      else {
+      } else {
         r = m - 1;
       }
     }
